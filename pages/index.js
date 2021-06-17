2a5d343f35c2837e8d51c3a6d74cdcb7646e4e72
Bryan C Guner <bryan.guner@gmail.com>
2021-06-17 19:16:40 +0000

    wub

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/bgoonz/web-module-project-deploying-web-apps"
           target="_blank"
           rel="noopener noreferrer"
         >
