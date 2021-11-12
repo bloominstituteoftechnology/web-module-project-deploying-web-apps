45687058fad9e888898d496bffbdbfe24c97653e
Nelson Maldonado <play2nino@gmail.com>
2021-11-11 23:53:01 -0500

    Random Meme generator

---
+++ random-meme-generator/src/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import reducer from "./reducer"
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+)
+
+reportWebVitals()
