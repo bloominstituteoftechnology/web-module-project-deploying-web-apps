20a3eaf6f01736dd4809f14cdf6fca066130e441
guest hugh <kbramall11@gmail.com>
2022-09-28 10:14:53 -0600

    I fixed some unused code

--- .history/demo-proj/src/App_20220928100456.js
+++ .history/demo-proj/src/App_20220928100456.js
@@ -1,10 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       I love writing
+       
       </header>
     </div>
   );
